62470d8a85366e895a76887cc4c820d94c9595b9
yosemitemike <mwillson@gmail.com>
2017-10-28 14:15:43 -0700

    Adding a slideshow at the bottom.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,35 @@
 $(document).ready(function() {
-    var getTime = function() {
-            var wedding = new Date("2017-11-11T23:30:00.000Z"),
-                now = new Date();
-                differenceInSeconds = (wedding.getTime() - now.getTime()) / 1000;
+    var initClock = function() {
+            var getTime = function() {
+                    var wedding = new Date("2017-11-11T23:30:00.000Z"),
+                        now = new Date();
+                        differenceInSeconds = (wedding.getTime() - now.getTime()) / 1000;
 
-            return Math.floor(differenceInSeconds);
+                    return Math.floor(differenceInSeconds);
+                },
+                clock = $('.clock').FlipClock({
+                    clockFace: 'DailyCounter',
+                    autoStart: false,
+                    callbacks: {
+                        stop: function() {
+                            console.log("The countdown ended");
+                        }
+                    }
+                }),
+                time = getTime();
+
+            clock.setTime(time);
+            clock.setCountdown(true);
+            clock.start();
         },
-        clock = $('.clock').FlipClock({
-            clockFace: 'DailyCounter',
-            autoStart: false,
-            callbacks: {
-                stop: function() {
-                    console.log("The countdown ended");
-                }
-            }
-        }),
-        time = getTime();
+        initSlides = function() {
+            var options = {
+                $AutoPlay: 1
+            };
 
-    clock.setTime(time);
-    clock.setCountdown(true);
-    clock.start();
+            new $JssorSlider$("slider1_container", options);
+        };
+
+    initClock();
+    initSlides();
 });
